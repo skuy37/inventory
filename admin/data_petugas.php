108743e8cca5285658841c80c9c9d3ccc2b12f32
mylittleunicorn <nasir.dev48@gmail.com>
2023-02-22 09:52:56 +0700

    form serch berhasil

--- admin/data_petugas.php
+++ admin/data_petugas.php
@@ -1,6 +1,7 @@
 <?php
-include ('../header.php');
-  ?>
+    include ('../header.php');
+
+?>
   
     <div class="container" style="margin-top: 80px; font-family: 'Righteous', cursive;">
       <div class="row">
@@ -9,74 +10,54 @@ include ('../header.php');
              DATA PETUGAS
             </div>
             <form method="GET" action="">
-              <label>Kata Pencarian : </label>
-              <input type="text" name="kata_cari" value="<?php if(isset($_GET['kata_cari'])) { echo $_GET['kata_cari']; } ?>"  />
-              <input type="hidden" class="form-control" name="page" required value="data_petugas">
-              <button type="submit">Cari</button>
+                <label>Kata Pencarian : </label>
+                <input type="text" name="kata_cari" />
+                <input type="hidden" class="form-control" name="page" required value="data_petugas">
+                <button type="submit" name="cari-petugas">Cari</button>
             </form>
             
               <table class="table table-bordered mt-3" id="myTable">
-                <thead>
-                  <tr>
-                    <th scope="col">NO.</th>
-                    <th scope="col">USERNAME</th>
-                    <th scope="col">EMAIL</th>
-                    <th scope="col">LEVEL</th>
-                    <th scope="col">PASSWORD</th>
-                    <th scope="col">AKSI</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php 
-                      include('../koneksi.php');
+                    <thead>
+                        <tr>
+                            <th scope="col">NO.</th>
+                            <th scope="col">USERNAME</th>
+                            <th scope="col">EMAIL</th>
+                            <th scope="col">LEVEL</th>
+                            <th scope="col">PASSWORD</th>
+                            <th scope="col">AKSI</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php 
+                    include('../koneksi.php');
+                    $no = 1;
+                    $data = mysqli_query($conn, "select * from users");		
+                    if(isset($_GET['kata_cari'])) {
+                        $kata_cari = $_GET['kata_cari']; 
+                        $data = mysqli_query($conn, "SELECT * FROM users WHERE username like '%".$kata_cari."%'");  
+                    } else { 
+                        $query = "SELECT * FROM users ORDER BY id ASC";
+                    }
+                     while ($d = mysqli_fetch_assoc($data)) {
+                    ?>
+                    <tr>
+                        <td><?php echo $no++ ?></td>
+                        <td><?php echo $d['username'] ?></td>
+                        <td><?php echo $d['email'] ?></td>
+                        <td><?php echo $d['level'] ?></td>
+                        <td><?php echo $d['password'] ?></td>
+                        <td class="text-center">
+                            <a href="menu.php?page=edit-petugas&id=<?php echo $d['id'] ?>" class="btn btn-sm btn-dark"><i class="bi bi-pen"></i>EDIT</a>
+                            <a href="hapus_petugas.php?id=<?php echo $d['id'] ?>" class="btn btn-sm btn-light" data-toggle="modal" data-target="#ModalLoginForm"><i class="bi bi-eraser"></i>HAPUS</a>
                      
-                   
-                      $no = 1;
-                      
-                         
-                        $data = mysqli_query($conn, "select * from users");		
-                    
-                      
-
-                        if(isset($_GET['kata_cari'])) {
-                          
-                          $kata_cari = $_GET['kata_cari'];   
-                
-                          
-                          $query = "SELECT * FROM users WHERE email like '%".$kata_cari."%' OR username like '%".$kata_cari."%' OR level like '%".$kata_cari."%' ORDER BY id ASC";
-                        } else {
-                          
-                          $query = "SELECT * FROM users ORDER BY id ASC";
-                        }
-                        
-                        while ($d = mysqli_fetch_assoc($data)) {
-                      
-                  ?>
-            
-
-                  <tr>
-                      <td><?php echo $no++ ?></td>
-                      <td><?php echo $d['username'] ?></td>
-                      <td><?php echo $d['email'] ?></td>
-                      <td><?php echo $d['level'] ?></td>
-                      <td><?php echo $d['password'] ?></td>
-                      <td class="text-center">
-                        <a href="menu.php?page=edit-petugas&id=<?php echo $d['id'] ?>" class="btn btn-sm btn-dark"><i class="bi bi-pen"></i>EDIT</a>
-                        <a href="hapus_petugas.php?id=<?php echo $d['id'] ?>" class="btn btn-sm btn-light" data-toggle="modal" data-target="#ModalLoginForm"><i class="bi bi-eraser"></i>HAPUS</a>
-                     
-                       
-
-
-                      </td>
-                  </tr>
-                
-                <?php 
-                      }
-                 ?>
-
-                </tbody>
-              </table>
-          </div>
+                        </td>
+                    </tr>
+                    <?php 
+                    }
+                    ?>
+                    </tbody>
+                </table>
+            </div>
       </div>
     </div>
 
