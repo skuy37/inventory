9bbf66580421391d7839d7de584c7bb14c9c98fc
skuy37 <donododo589@gmail.com>
2023-03-07 09:10:44 +0700

    validasi

--- admin/tambah.php
+++ admin/tambah.php
@@ -1,11 +1,7 @@
 <?php
 include('../header.php');
-if(isset($_GET['nama_barang'])){
-  if($_GET['nama_barang'] == "kosong"){
-      echo "<h4 style='color:red'>Harap ketik nama pada kolom tersedia !</h4>";
-  }
-}
 ?>
+
 
     <div class="container" style="margin-top: 80px; font-family: 'Righteous', cursive;">
       <div class="row">
@@ -15,20 +11,20 @@ if(isset($_GET['nama_barang'])){
               TAMBAH DATA
             </div>
             <div class="card-body">
-              <form action="menu.php?page=simpan" method="POST" onSubmit="validasi()">
+              <form action="menu.php?page=simpan" method="POST">
                 
                 <div class="form-group">
                   <label>NAMA BARANG</label>
-                  <input type="text" name="nama_barang" placeholder="Masukkan Nama Barang" class="form-control">
+                  <input type="text" name="nama_barang" placeholder="Masukkan Nama Barang" class="form-control" required>
                 </div><br>
 
                 <div class="form-group">
                   <label>KODE BARANG</label>
-                  <input type="text" name="kode_barang" placeholder="Masukkan Kode Barang " class="form-control">
+                  <input type="text" name="kode_barang" placeholder="Masukkan Kode Barang " class="form-control" required>
                 </div><br>
                 <div class="form-group">
                   <label>LABORATORIUM</label>
-                  <select name="lab" id="" class="form-control">
+                  <select name="lab" id="" class="form-control" required>
                     <option></option>
                     <option>LAB 1</option>
                     <option>LAB 2</option>
@@ -37,7 +33,7 @@ if(isset($_GET['nama_barang'])){
                 </div><br>
                 <div class="form-group">
                   <label>RUANGAN</label>
-                  <select name="ruangan" id="" class="form-control">
+                  <select name="ruangan" id="" class="form-control" required>
                     <option></option>
                     <option>R1</option>
                     <option>R2</option>
@@ -46,11 +42,11 @@ if(isset($_GET['nama_barang'])){
                 </div><br>
                 <div class="form-group">
                   <label>TANGGAL MASUK</label>
-                  <input type="date" name="tgl_masuk" placeholder="Masukkan Nama Siswa" class="form-control">
+                  <input type="date" name="tgl_masuk" placeholder="Masukkan Nama Siswa" class="form-control" required>
                 </div><br>
                 <div class="form-group">
                   <label>KONDISI</label>
-                  <select name="kondisi" id="" class="form-control">
+                  <select name="kondisi" id="" class="form-control" required>
                     <option>BAIK</option>
                     <option>RUSAK RINGAN</option>
                     <option>RUSAK BERAT</option>
@@ -67,18 +63,7 @@ if(isset($_GET['nama_barang'])){
         </div>
       </div>
     </div>
-    <script type="text/javascript">
-            function validasi() {
-              var nama_barang = document.getElementById("nama_barang").value;
-              var kode_barang = document.getElementById("kode_barang").value;
-              var ko = document.getElementById("kode_barang").value;
-              if (nama_barang != "" && kode_barang!="") {
-                return true;
-              }else{
-                alert('Anda harus mengisi data dengan lengkap !');
-              }
-            }
-</script>
+
     <?php
 include('../footer.php');
 ?>
